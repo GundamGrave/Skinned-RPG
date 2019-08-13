ab4578f5212609acce7a45309f06533cd98d84d7
GundamGrave <obscuredartanddesign@gmail.com>
2019-08-13 20:10:24 +0930

    Models Added and Inventory scriptable items created

---
+++ Assets/Scripts/SFX/XTurner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XTurner : MonoBehaviour
+{
+    public float speed = 90;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(0, Time.time * speed, 0);
+    }
+}
+
