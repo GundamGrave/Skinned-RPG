915a4ea26436370784b61417c041689eeb948d55
obscuredartanddesign@gmail.com <obscuredartanddesign@gmail.com>
2019-10-22 19:06:08 +1030

    Main, Pause and Game over screens added and working

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    private const string MainMenu = "Main Menu";
+    public static bool GameOverScreen = false;
+
+    public GameObject gameOverUI;
+
+    void Update()
+    {
+        if (GameOverScreen)
+        {
+            Pause();
+        }
+    }
+
+    void Pause()
+    {
+        gameOverUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameOverScreen = true;
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Restart Game");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level 001");
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Load Menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("quit game...");
+        Application.Quit();
+    }
+}
